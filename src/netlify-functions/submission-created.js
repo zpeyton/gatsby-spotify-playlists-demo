edbd8a1780a6cb86a417704fb45bc5e34d49f2be
Zach Peyton <zachary.peyton@gmail.com>
2019-10-20 16:56:23 -0400

    more logging

--- src/netlify-functions/submission-created.js
+++ src/netlify-functions/submission-created.js
@@ -6,11 +6,12 @@ const { SENDGRID_API_KEY } = process.env
 exports.handler = function(event,context,callback) {
     
     //console.log(`Sendgrid API Key ${SENDGRID_API_KEY}`)
-    console.log("v5")
+    console.log("v6")
 
     let jsonData = JSON.parse(event.body)
     let request = jsonData.payload
     
+    console.log(request['form-name'])
     if(request['form-name'] == 'contact'){
         const email = request.email
         console.log(`Recieved a submission: ${email}`)
