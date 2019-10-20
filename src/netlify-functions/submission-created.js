cc530360dcd33bdc6232e5f3ac0c7dc28a7373bc
Zach Peyton <zachary.peyton@gmail.com>
2019-10-20 16:37:59 -0400

    almost there with the functions

--- src/netlify-functions/submission-created.js
+++ src/netlify-functions/submission-created.js
@@ -6,10 +6,12 @@ const { SENDGRID_API_KEY } = process.env
 exports.handler = function(event,context,callback) {
     
     //console.log(`Sendgrid API Key ${SENDGRID_API_KEY}`)
-    console.log("v2")
+    console.log("v3")
     
     //console.log(event.body)
-    let jsonData = JSON.parse(event.body) 
+    let jsonData = JSON.parse(event.body)
+    console.log(jsonData.email) 
+    
     if(jsonData.payload['form-name'] == 'contact'){
         const email = jsonData.payload.email
         console.log(`Recieved a submission: ${email}`)
