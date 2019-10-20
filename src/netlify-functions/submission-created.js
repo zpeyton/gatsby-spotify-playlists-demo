94762b99a7f64b53fa7ef676b483a9c18e661234
Zach Peyton <zachary.peyton@gmail.com>
2019-10-20 15:42:48 -0400

    Form Submission Netlify function

---
+++ src/netlify-functions/submission-created.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+// const fetch = require('node-fetch')
+
+exports.handler = async (event) => {
+    console.log(event.body)
+    let jsonData = JSON.parse(event.body) 
+    if(jsonData.payload['form-name'] == 'contact'){
+        const email = JSON.parse(event.body).payload.email
+        console.log(`Recieved a submission: ${email}`)
+    }
+    
+    return true
+}
