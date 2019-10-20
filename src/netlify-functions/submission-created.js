0e2fba13f801788f653f45c9665b38029daeb356
Zach Peyton <zachary.peyton@gmail.com>
2019-10-20 17:22:01 -0400

    integrate sendgrid

--- src/netlify-functions/submission-created.js
+++ src/netlify-functions/submission-created.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 // const fetch = require('node-fetch')
+const sendgrid = require('@sendgrid/mail')
+
 const { SENDGRID_API_KEY } = process.env
+
+sendgrid.setApiKey(SENDGRID_API_KEY);
 
 exports.handler = function(event,context,callback) {
     
@@ -11,7 +15,17 @@ exports.handler = function(event,context,callback) {
     let request = jsonData.payload
     
     if(request.form_name == 'contact'){
-        const email = request.email
+        
+        const { name, email, phone, message } = request
+        
+        let mailParams = {
+            to: 'zachary.peyton@gmail.com',
+            from: 'Web form <admin@quizmaniapp.com>',
+            replyTo: `${name} <${email}>`,
+            subject: 'Contact Form',
+            text: `${name} - ${email}\n\n ${message}`
+        }
+        sendgrid.send(mailParams)
         console.log(`Recieved a submission: ${email}`)
     }
 
