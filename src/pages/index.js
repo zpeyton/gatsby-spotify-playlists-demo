0b2a99af9771cda00414c87bf42db3d6074593ad
Zach Peyton <zachary.peyton@gmail.com>
2019-10-20 14:31:37 -0400

    Remove form action

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
+  /*
+    action="/contact-success"
+  */
   <Layout>
     <SEO title="Home" />
     <h1>Contact us</h1>
     <p>This form should theoretically submit data to Netlify</p>
-    <form netlify name="contact" id="contact" method="post" action="/contact-success">
+    <form netlify name="contact" id="contact" method="post">
       <input name="name" placeholder="Name"></input>
       <input name="email" placeholder="Email"></input>
       <input name="phone" placeholder="Phone"></input>
