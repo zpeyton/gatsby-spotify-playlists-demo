e9b9ae4d9685133e5c6f81f09dfb5d220961f71f
Zach Peyton <zachary.peyton@gmail.com>
2019-10-20 15:02:44 -0400

    Must add hidden form-name field if using a static site generator

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,10 @@ const IndexPage = () => (
     <h1>Contact us</h1>
     <p>This form should theoretically submit data to Netlify</p>
     <form data-netlify="true" name="contact" id="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
       <input type="text" name="name" placeholder="Name" />
       <input type="text" name="email" placeholder="Email" />
-      <input type="text" name="cell" placeholder="Cell" />
+      <input type="text" name="phone" placeholder="Phone" />
       <textarea name="message" placeholder="Message"></textarea>
       <br />
       <button type="submit">Send Message</button>
