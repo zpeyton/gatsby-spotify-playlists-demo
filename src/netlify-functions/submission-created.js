0718de2342db0ffb8d3b755f36fa13c3050478cf
Zach Peyton <zachary.peyton@gmail.com>
2019-10-20 17:09:12 -0400

    should work now

--- src/netlify-functions/submission-created.js
+++ src/netlify-functions/submission-created.js
@@ -2,17 +2,15 @@ require('dotenv').config()
 // const fetch = require('node-fetch')
 const { SENDGRID_API_KEY } = process.env
 
-
 exports.handler = function(event,context,callback) {
     
     //console.log(`Sendgrid API Key ${SENDGRID_API_KEY}`)
-    console.log("v6")
+    console.log("v7")
 
     let jsonData = JSON.parse(event.body)
     let request = jsonData.payload
     
-    console.log(request['form-name'])
-    if(request['form-name'] == 'contact'){
+    if(request.form_name == 'contact'){
         const email = request.email
         console.log(`Recieved a submission: ${email}`)
     }
