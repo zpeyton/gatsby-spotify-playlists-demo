d83fbfa9b5101078eef95feccd76353a4784c639
Zach Peyton <zachary.peyton@gmail.com>
2019-10-20 16:41:54 -0400

    figuring out event body vars

--- src/netlify-functions/submission-created.js
+++ src/netlify-functions/submission-created.js
@@ -6,11 +6,12 @@ const { SENDGRID_API_KEY } = process.env
 exports.handler = function(event,context,callback) {
     
     //console.log(`Sendgrid API Key ${SENDGRID_API_KEY}`)
-    console.log("v3")
+    console.log("v4")
     
     //console.log(event.body)
     let jsonData = JSON.parse(event.body)
-    console.log(jsonData.email) 
+    console.log(jsonData.data)
+    console.log(jsonData.payload) 
     
     if(jsonData.payload['form-name'] == 'contact'){
         const email = jsonData.payload.email
